ddb1b9f99106d96c1f69da7c3a3d0080a4c750ee
linga-siva <lingasiva.p@zohocorp.com>
2026-02-18 00:36:51 +0530

    test change

--- src/main/java/com/example/petclinic/PetClinicApplication.java
+++ src/main/java/com/example/petclinic/PetClinicApplication.java
@@ -10,7 +10,14 @@ public class PetClinicApplication {
         SpringApplication.run(PetClinicApplication.class, args);
         System.out.println("what");
         System.out.println("will it work");
-      
+        try{
+        int divideByZero = 0/3;
+      }
+      catch(Exception e)
+      {
+        System.out.println("divide by zero error");
+
+      }
 
     }
 
